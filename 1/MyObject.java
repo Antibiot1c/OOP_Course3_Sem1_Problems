57a5a5a0bac0e968caf1e463a73372817b432ddf
Pavlo Velychko <112926321+Antibiot1c@users.noreply.github.com>
2023-12-01 21:24:21 +0200

    Create MyObject.java

---
+++ 1/MyObject.java
@@ -0,0 +1,17 @@
+package clientserver;
+
+import java.io.Serializable;
+
+public class MyObject implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String message;
+
+    public MyObject(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
